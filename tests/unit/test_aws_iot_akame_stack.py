b6d19e36a09f576f8ea7562f5f2af36ea988d61b
cnxontle <cnxontle@gmail.com>
2025-11-16 12:37:46 -0600

    Initial commit

---
+++ tests/unit/test_aws_iot_akame_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from aws_iot_akame.aws_iot_akame_stack import AwsIotAkameStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in aws_iot_akame/aws_iot_akame_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = AwsIotAkameStack(app, "aws-iot-akame")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
